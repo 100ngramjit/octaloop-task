b564db49995414c35d1d8833ca98c59eaa92f00f
100ngramjit <sangramjitdutta@gmail.com>
2022-12-30 00:29:21 +0530

    Dashboard Content layout added

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -15,192 +15,179 @@ import icon3 from "../assets/item3.png";
 import icon4 from "../assets/item4.png";
 import icon5 from "../assets/item5.png";
 import icon6 from "../assets/item6.png";
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
 
 const DashboardSidebar = () => {
   return (
-    <Box sx={{ display: "flex" }}>
-      <Drawer variant="permanent">
-        <Box sx={{ justifyContent: "space-between" }}>
-          <List>
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    justifyContent: "center",
-                  }}
-                >
-                  <Avatar
-                    alt="logo"
-                    src={logo}
-                    style={{ width: 30, height: 30, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    alt="icon2"
-                    src={icon2}
-                    style={{ width: 35, height: 50, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    alt="icon3"
-                    src={icon3}
-                    style={{ width: 35, height: 50, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>{" "}
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    alt="icon4"
-                    src={icon4}
-                    style={{ width: 35, height: 50, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>{" "}
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    alt="icon5"
-                    src={icon5}
-                    style={{ width: 35, height: 50, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>{" "}
-          </List>
-          <List>
-            <ListItem disablePadding sx={{ display: "block", mt: 30 }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img
-                    alt="icon6"
-                    src={icon6}
-                    style={{ width: 35, height: 50, my: 2 }}
-                  />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <List>
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                justifyContent: "center",
+              }}
+            >
+              <Avatar
+                alt="logo"
+                src={logo}
+                style={{ width: 30, height: 30, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon1"
+                src={icon1}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon2"
+                src={icon2}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon3"
+                src={icon3}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>{" "}
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon4"
+                src={icon4}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>{" "}
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon5"
+                src={icon5}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>{" "}
+      </List>
+      <List>
+        <ListItem disablePadding sx={{ display: "block", mt: 20 }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: "auto",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                alt="icon6"
+                src={icon6}
+                style={{ width: 35, height: 50, my: 2 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
